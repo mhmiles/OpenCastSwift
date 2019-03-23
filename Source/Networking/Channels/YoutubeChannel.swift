aafb78a0f4e1c353133d72e4e2a5877697fac8a2
Levi McCallum <levi@levimccallum.com>
2019-03-23 15:24:29 -0700

    Now building

--- Source/Networking/Channels/YoutubeChannel.swift
+++ Source/Networking/Channels/YoutubeChannel.swift
@@ -20,73 +20,93 @@ enum YoutubeAction: String {
 
 class YoutubeChannel: CastChannel {
   
-  static let BIND_URL = URL(string: "https://www.youtube.com/api/lounge/bc/bind")!
+  private static let YOUTUBE_BASE_URL = "https://www.youtube.com/"
+  private static let BIND_URL = URL(string: "\(YOUTUBE_BASE_URL)api/lounge/bc/bind")!
+  private static let LOUNGE_TOKEN_URL = URL(string: "\(YOUTUBE_BASE_URL)api/lounge/pairing/get_lounge_token_batch")!
   
-  static let LOUNGE_TOKEN_URL = URL(string: "https://www.youtube.com/api/lounge/pairing/get_lounge_token_batch")!
+  private let CURRENT_INDEX = "_currentIndex"
+  private let CURRENT_TIME = "_currentTime"
+  private let AUDIO_ONLY = "_audioOnly"
+  private let VIDEO_ID = "_videoId"
+  private let LIST_ID = "_listId"
+  private let ACTION = "__sc"
+  private let COUNT = "count"
 
+  private let LOUNGE_ID_HEADER = "X-YouTube-LoungeId-Token"
+  
+  private let GSESSIONID = "gsessionid"
+  private let CVER = "CVER"
+  private let RID = "RID"
+  private let SID = "SID"
+  private let VER = "VER"
+
+  /// Current request id
   private var rid: Int = 0
   
+  /// Current number of requests performed
   private var reqCount: Int = 0
   
   private var sid: Int = 0
   
-  private var screenID: String = ""
+  // TODO: How to set the screenid without the initializer?
+  private var screenID: String? = nil
   
-  private var gsessionID: Int? = nil
+  private var gsessionID: String = ""
   
-  private var loungeToken: String? = nil
+  private var loungeToken: String = ""
 
   init() {
     super.init(namespace: CastNamespace.youtube)
   }
 
-  public func playVideo(_ videoID: String, playlistID: String? = nil) {
-    fetchScreenIDIfNecessary { result in
+  public func playVideo(for app: CastApp, videoID: String, playlistID: String? = nil) {
+    fetchScreenIDIfNecessary(for: app) { result in
       switch result {
       case .success:
-        self.startSession()
-        self.initializeQueue(videoID: videoID, playlistID: playlistID)
+        self.startSession() {
+          self.initializeQueue(videoID: videoID, playlistID: playlistID)
+        }
       case .failure(let error):
         print(error)
       }
     }
   }
 
-  public func addToQueue(_ videoID: String) {
-    fetchScreenIDIfNecessary { result in
+  public func addToQueue(for app: CastApp, videoID: String) {
+    fetchScreenIDIfNecessary(for: app) { result in
       switch result {
       case .success:
-        self.queueAction(.add, videoID)
+        self.queueAction(.add, videoID: videoID)
       case .failure(let error):
         print(error)
       }
     }
   }
 
-  public func playNext(_ videoID: String) {
-    fetchScreenIDIfNecessary { result in
+  public func playNext(for app: CastApp, videoID: String) {
+    fetchScreenIDIfNecessary(for: app) { result in
       switch result {
       case .success:
-        self.queueAction(.insert, videoID)
+        self.queueAction(.insert, videoID: videoID)
       case .failure(let error):
         print(error)
       }
     }
   }
 
-  public func removeVideo(_ videoID: String) {
-    fetchScreenIDIfNecessary { result in
+  public func removeVideo(for app: CastApp,videoID: String) {
+    fetchScreenIDIfNecessary(for: app) { result in
       switch result {
       case .success:
-        self.queueAction(.remove, videoID)
+        self.queueAction(.remove, videoID: videoID)
       case .failure(let error):
         print(error)
       }
     }
   }
 
-  public func clearPlaylist() {
-    fetchScreenIDIfNecessary { result in
+  public func clearPlaylist(for app: CastApp) {
+    fetchScreenIDIfNecessary(for: app) { result in
       switch result {
       case .success:
         self.queueAction(.clear)
@@ -96,9 +116,12 @@ class YoutubeChannel: CastChannel {
     }
   }
 
-  private func startSession() {
-    getLoungeID()
-    bind()
+  private func startSession(_ completion: @escaping () -> Void) {
+    getLoungeID() {
+      self.bind() {
+        completion()
+      }
+    }
   }
 
   /**
@@ -106,12 +129,15 @@ class YoutubeChannel: CastChannel {
 
     The token is used as a header in all session requests
   */
-  private func getLoungeID() {
-    postRequest(YoutubeChannel.LOUNGE_TOKEN_URL, data: ["screen_ids": screenID]) { result in
+  private func getLoungeID(_ completion: @escaping () -> Void) {
+    guard let screenID = screenID else { return }
+    postRequest(YoutubeChannel.LOUNGE_TOKEN_URL, data: ["screen_ids": screenID]) {
+      result in
       switch result {
       case .success(let response):
         // TODO: Traverse json response
-        self.loungeToken = response.json()["screens"][0]["loungeToken"]
+//        self.loungeToken = response.json()["screens"][0]["loungeToken"]
+        completion()
       case .failure(let error):
         // TODO: Handle error
         print(error)
@@ -127,7 +153,7 @@ class YoutubeChannel: CastChannel {
 
     SID, gsessionid are used as url params in all further session requests.
    */
-  private func bind() {
+  private func bind(_ completion: @escaping () -> Void) {
     rid = 0
     reqCount = 0
 
@@ -139,15 +165,18 @@ class YoutubeChannel: CastChannel {
       "pairing_type": "cast",
       "app": "android-phone-13.14.55"
     ]
-    let headers: [String: Any] = [ "X-YouTube-LoungeId-Token": loungeToken ]
-    let urlParams: [String: Any] = ["RID": rid, "VER": 8, "CVER": 1]
-    postRequest(YoutubeChannel.BIND_URL, data: data, headers: headers, params: urlParams) {
-      result in
+
+    let headers: [String: String] = [ LOUNGE_ID_HEADER: loungeToken ]
+    let urlParams: [String: String] = [ RID: "\(rid)", VER: "8", CVER: "1" ]
+
+    postRequest(YoutubeChannel.BIND_URL, data: data, headers: headers, params: urlParams) { result in
       switch result {
       case .success(let response):
-        var content = str(response.content)
-        self.sid = re.search(SID_REGEX, content)
-        self.gsessionID = re.search(GSESSION_ID_REGEX, content)
+        // TODO: Implement the regex
+//        var content = str(response.content)
+//        self.sid = re.search(SID_REGEX, content)
+//        self.gsessionID = re.search(GSESSION_ID_REGEX, content)
+        completion()
       case .failure(let error):
         // TODO: Handle error
         print(error)
@@ -158,25 +187,25 @@ class YoutubeChannel: CastChannel {
   /**
     Initialize a queue with a video and start playing that video.
   */
-  private func initializeQueue(videoID: String, listID: String = "") {
+  private func initializeQueue(videoID: String, playlistID: String?) {
     let data = formatSessionParams([
-      "LIST_ID": listID,
-      "ACTION": YoutubeAction.set.rawValue,
-      "CURRENT_TIME": "0",
-      "CURRENT_INDEX": -1,
-      "AUDIO_ONLY": "false",
-      "VIDEO_ID": videoID,
-      "COUNT": 1,
+      LIST_ID: playlistID ?? "",
+      ACTION: YoutubeAction.set.rawValue,
+      CURRENT_TIME: "0",
+      CURRENT_INDEX: -1,
+      AUDIO_ONLY: "false",
+      VIDEO_ID: videoID,
+      COUNT: 1,
     ])
     
-    let headers: [String: Any] = [ "X-YouTube-LoungeId-Token": loungeToken ]
+    let headers: [String: String] = [ LOUNGE_ID_HEADER: loungeToken ]
 
-    let params = [
-      "SID": sid,
-      "GSESSIONID": gsessionID,
-      "RID": rid,
-      "VER": 8,
-      "CVER": 1
+    let params: [String: String] = [
+      SID: "\(sid)",
+      GSESSIONID: gsessionID,
+      RID: "\(rid)",
+      VER: "8",
+      CVER: "1"
     ]
 
     postRequest(
@@ -191,47 +220,77 @@ class YoutubeChannel: CastChannel {
   /**
     Sends actions for an established queue.
   */
-  private func queueAction(action: YoutubeAction, videoID: String = "") {
+  private func queueAction(_ action: YoutubeAction, videoID: String = "") {
+    let performAction = {
+      let requestData = self.formatSessionParams([
+        self.ACTION: action,
+        self.VIDEO_ID: videoID,
+        self.COUNT: 1,
+      ])
+      
+      let urlParams: [String: String] = [
+        self.SID: "\(self.sid)",
+        self.GSESSIONID: self.gsessionID,
+        self.RID: "\(self.rid)",
+        self.VER: "8",
+        self.CVER: "1",
+      ]
+      
+      let headers = [
+        self.LOUNGE_ID_HEADER: self.loungeToken
+      ]
+      
+      self.postRequest(
+        YoutubeChannel.BIND_URL,
+        data: requestData,
+        headers: headers,
+        params: urlParams,
+        sessionRequest: true
+      )
+    }
+
     // If nothing is playing actions will work but won"t affect the queue.
     // This is for binding existing sessions
     if inSession == false {
-      startSession()
+      startSession() {
+        performAction()
+      }
     } else {
       // There is a bug that causes session to get out of sync after about 30 seconds. Binding again works.
       // Binding for each session request has a pretty big performance impact
-      bind()
+      bind() {
+        performAction()
+      }
     }
-
-    let requestData = formatSessionParams([
-      "ACTION": action,
-      "VIDEO_ID": videoID,
-      "COUNT": 1,
-    ])
-
-    let urlParams: [String: Any] = [
-      "SID": sid,
-      "GSESSIONID": gsessionID,
-      "RID": rid,
-      "VER": 8,
-      "CVER": 1,
-    ]
-    
-    let headers = [
-      "X-YouTube-LoungeId-Token": loungeToken
-    ]
-
-    postRequest(BIND_URL, data: requestData, params: urlParams, headers: headers, sessionRequest: true)
   }
   
   private func postRequest(
     _ url: URL,
-    data: [String: String],
+    data: [String: Any],
     headers: [String: String]? = nil,
     params: [String: String]? = nil,
-    sessionRequest: Bool = false
+    sessionRequest: Bool = false,
+    completion: ((Result<Any, NSError>) -> Void)? = nil
   ) {
-    var request = MutableRequest(url: url)
-    request.HTTPMethod = "POST"
+    var request: URLRequest
+
+    if let params = params {
+      let queryItems = params.map { (args) -> URLQueryItem in
+        let (key, value) = args
+        return URLQueryItem(name: key, value: value)
+      }
+      var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+      urlComponents?.queryItems = queryItems
+      if let paramURL = urlComponents?.url {
+        request = URLRequest(url: paramURL)
+      } else {
+        return
+      }
+    } else {
+      request = URLRequest(url: url)
+    }
+
+    request.httpMethod = "POST"
     request.setValue("https://www.youtube.com/", forHTTPHeaderField: "Origin")
     request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 
@@ -241,48 +300,62 @@ class YoutubeChannel: CastChannel {
       }
     }
     
-    let dataParts = data.map { (key, value) -> String in
+    let dataParts = data.map { (arg) -> String in
+      let (key, value) = arg
       return "\(key)=\(self.percentEscapeString(value))"
     }
-
     request.httpBody = dataParts.joined(separator: "&").data(using: String.Encoding.utf8)
     
     let session = URLSession.shared
     session.dataTask(with: request) { (data, response, error) in
-      // TODO: Handle error
-      if let data = data {
-        do {
-          let json = try JSONSerialization.jsonObject(with: data, options: [])
-          print(json)
-        } catch {
-          print(error)
-        }
+      guard error == nil && data != nil else {
+        completion?(.failure(error! as NSError))
+        return
+      }
+
+      guard let data = data else {
+        return
+      }
+
+      do {
+        let json = try JSONSerialization.jsonObject(with: data, options: [])
+        print(json)
+      } catch {
+        print(error)
       }
     }.resume()
   }
   
   private var inSession: Bool {
-    return (gsessionID != nil && loungeToken != nil)
+    return (gsessionID != "" && loungeToken != "")
   }
   
-  private func percentEscapeString(string: String) -> String {
+  private func percentEscapeString(_ string: Any) -> String {
     var characterSet = CharacterSet.alphanumerics
     characterSet.insert(charactersIn: "-._* ")
     
-    return string
+    return "\(string)"
       .addingPercentEncoding(withAllowedCharacters: characterSet)!
       .replacingOccurrences(of: " ", with: "+")
       .replacingOccurrences(of: " ", with: "+", options: [], range: nil)
   }
 
   private func formatSessionParams(_ params: [String: Any]) -> [String: Any] {
-    var reqCount = "req\(self.reqCount)"
-//    return {req_count + k if k.startswith("_") else k: v for k, v in param_dict.items()}
+    let reqCount = "req\(self.reqCount)"
+    var ret = [String: Any]()
+    for (key, value) in params {
+      if (key.starts(with: "_")) {
+        ret["\(reqCount)\(key)"] = value
+      } else {
+        ret[key] = value
+      }
+    }
+    return ret
   }
 
   private func fetchScreenIDIfNecessary(for app: CastApp, completion: @escaping (Result<String, CastError>) -> Void) {
     if let screenID = screenID {
-      completion(Result(success: screenID))
+      completion(.success(screenID))
       return
     }
 
@@ -300,9 +373,10 @@ class YoutubeChannel: CastChannel {
     send(request) { result in
       switch result {
       case .success(let json):
-        guard let screenId = json["data"]?["screenId"] else { return }
-        self.screenID = screenId
-        completion(Result(success: screenId))
+        print(json)
+//        guard let screenID = json["data"]?["screenId"] else { return }
+//        self.screenID = screenID
+//        completion(Result(success: screenID))
       case .failure(let error):
         completion(Result(error: CastError.load(error.localizedDescription)))
       }
