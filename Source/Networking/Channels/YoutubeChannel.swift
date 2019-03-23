357f5c0612177c017f869637dadcbb774e5eee6b
Levi McCallum <levi@levimccallum.com>
2019-03-23 00:05:10 -0700

    WIP

--- Source/Networking/Channels/YoutubeChannel.swift
+++ Source/Networking/Channels/YoutubeChannel.swift
@@ -10,7 +10,32 @@ import Foundation
 import Result
 import SwiftyJSON
 
+enum YoutubeAction: String {
+  case add = "addVideo"
+  case insert = "insertVideo"
+  case remove = "removeVideo"
+  case set = "setPlaylist"
+  case clear = "clearPlaylist"
+}
+
 class YoutubeChannel: CastChannel {
+  
+  static let BIND_URL = URL(string: "https://www.youtube.com/api/lounge/bc/bind")!
+  
+  static let LOUNGE_TOKEN_URL = URL(string: "https://www.youtube.com/api/lounge/pairing/get_lounge_token_batch")!
+
+  private var rid: int = 0
+  
+  private var reqCount: int = 0
+  
+  private var sid: int = 0
+  
+  private var screenID: String = ""
+  
+  private var gsessionID: Int? = nil
+  
+  private var loungeToken: String? = nil
+  
   private var delegate: YoutubeChannelDelegate? {
     return requestDispatcher as? YoutubeChannelDelegate
   }
@@ -20,24 +45,59 @@ class YoutubeChannel: CastChannel {
   }
 
   public func playVideo(_ videoID: String, playlistID: String? = nil) {
-    startSession()
-    initializeQueue(videoID, playlistID)
+    fetchScreenIDIfNecessary { result in
+      switch result {
+      case .success:
+        self.startSession()
+        self.initializeQueue(videoID: videoID, playlistID: playlistID)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   public func addToQueue(_ videoID: String) {
-    queueAction(videoID, "addVideo")
+    fetchScreenIDIfNecessary { result in
+      switch result {
+      case .success:
+        self.queueAction(.add, videoID)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   public func playNext(_ videoID: String) {
-    queueAction(videoID, "insertVideo")
+    fetchScreenIDIfNecessary { result in
+      switch result {
+      case .success:
+        self.queueAction(.insert, videoID)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   public func removeVideo(_ videoID: String) {
-    queueAction(videoID, "removeVideo")
+    fetchScreenIDIfNecessary { result in
+      switch result {
+      case .success:
+        self.queueAction(.remove, videoID)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   public func clearPlaylist() {
-    queueAction("", "clearPlaylist")
+    fetchScreenIDIfNecessary { result in
+      switch result {
+      case .success:
+        self.queueAction(.clear)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
 
   private func startSession() {
@@ -51,18 +111,144 @@ class YoutubeChannel: CastChannel {
     The token is used as a header in all session requests
   */
   private func getLoungeID() {
-    let url = NSURL(string: "https://www.youtube.com/api/lounge/pairing/get_lounge_token_batch")!
+    postRequest(LOUNGE_TOKEN_URL, data: ["screen_ids": screenID]) { result in
+      switch result {
+      case .success(let response):
+        // TODO: Traverse json response
+        self.loungeToken = response.json()["screens"][0]["loungeToken"]
+      case .failure(let error):
+        // TODO: Handle error
+        print(error)
+      }
+    }
+  }
 
-    let parameterDictionary = ["screen_ids": screenID]
+  /**
+    Bind to the app and get SID, gsessionid session identifiers.
 
+    If the chromecast is already in another YouTube session you should get
+    the SID, gsessionid for that session.
+
+    SID, gsessionid are used as url params in all further session requests.
+   */
+  private func bind() {
+    rid = 0
+    reqCount = 0
+
+    let data: [String: Any] = [
+      "device": "REMOTE_CONTROL",
+      "id": "aaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "name": "OpenCastSwift",
+      "mdx-version": 3,
+      "pairing_type": "cast",
+      "app": "android-phone-13.14.55"
+    ]
+    let headers: [String: Any] = [ "X-YouTube-LoungeId-Token": loungeToken ]
+    let urlParams: [String: Any] = ["RID": rid, "VER": 8, "CVER": 1]
+    postRequest(YoutubeChannel.BIND_URL, data: data, headers: headers, params: urlParams) {
+      result in
+      switch result {
+      case .success(let response):
+        var content = str(response.content)
+        self.sid = re.search(SID_REGEX, content)
+        self.gsessionID = re.search(GSESSION_ID_REGEX, content)
+      case .failure(let error):
+        // TODO: Handle error
+        print(error)
+      }
+    }
+  }
+
+  /**
+    Initialize a queue with a video and start playing that video.
+  */
+  private func initializeQueue(videoID: String, listID: String = "") {
+    let data = formatSessionParams([
+      "LIST_ID": listID,
+      "ACTION": YoutubeAction.set.rawValue,
+      "CURRENT_TIME": "0",
+      "CURRENT_INDEX": -1,
+      "AUDIO_ONLY": "false",
+      "VIDEO_ID": videoID,
+      "COUNT": 1,
+    ])
+    
+    let headers: [String: Any] = [ "X-YouTube-LoungeId-Token": loungeToken ]
+
+    let params = [
+      "SID": sid,
+      "GSESSIONID": gsessionID,
+      "RID": rid,
+      "VER": 8,
+      "CVER": 1
+    ]
+
+    postRequest(
+      BIND_URL,
+      data: data,
+      headers: headers,
+      params: params,
+      sessionRequest: true
+    )
+  }
+
+  /**
+    Sends actions for an established queue.
+  */
+  private func queueAction(action: YoutubeAction, videoID: String = "") {
+    // If nothing is playing actions will work but won"t affect the queue.
+    // This is for binding existing sessions
+    if inSession == false {
+      startSession()
+    } else {
+      // There is a bug that causes session to get out of sync after about 30 seconds. Binding again works.
+      // Binding for each session request has a pretty big performance impact
+      bind()
+    }
+
+    let requestData = formatSessionParams([
+      "ACTION": action,
+      "VIDEO_ID": videoID,
+      "COUNT": 1,
+    ])
+
+    let urlParams: [String: Any] = [
+      "SID": sid,
+      "GSESSIONID": gsessionID,
+      "RID": rid,
+      "VER": 8,
+      "CVER": 1,
+    ]
+    
+    let headers = [
+      "X-YouTube-LoungeId-Token": loungeToken
+    ]
+
+    postRequest(BIND_URL, data: requestData, params: urlParams, headers: headers, sessionRequest: true)
+  }
+  
+  private func postRequest(
+    _ url: URL,
+    data: [String: String],
+    headers: [String: String]? = nil,
+    params: [String: String]? = nil,
+    sessionRequest: Bool = false
+  ) {
     var request = MutableRequest(url: url)
     request.HTTPMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    guard let httpBody = try? JSONSerialization.data(withJSONObject: params, options: []) else {
+    request.setValue("https://www.youtube.com/", forHTTPHeaderField: "Origin")
+    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    if let headers = headers {
+      for (k, v) in headers {
+        request.setValue(v, forHTTPHeaderField: k)
+      }
+    }
+
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: data, options: []) else {
       return
     }
     request.httpBody = httpBody
-
+    
     let session = URLSession.shared
     session.dataTask(with: request) { (data, response, error) in
       if let response = response {
@@ -78,192 +264,39 @@ class YoutubeChannel: CastChannel {
       }
     }.resume()
   }
-
-  /**
-    Bind to the app and get SID, gsessionid session identifiers.
-
-    If the chromecast is already in another YouTube session you should get
-    the SID, gsessionid for that session.
-
-    SID, gsessionid are used as url params in all further session requests.
-   */
-  private func bind() {
-    rid = 0
-    reqCount = 0
-
-    url_params = {RID: self._rid, VER: 8, CVER: 1}
-    headers = {LOUNGE_ID_HEADER: self._lounge_token}
-    response = self._do_post(BIND_URL, data=BIND_DATA, headers=headers,
-                              params=url_params)
-    content = str(response.content)
-    sid = re.search(SID_REGEX, content)
-    gsessionid = re.search(GSESSION_ID_REGEX, content)
-    self._sid = sid.group(1)
-    self._gsession_id = gsessionid.group(1)
-  }
-
-  /**
-    Initialize a queue with a video and start playing that video.
-  */
-  private func initializeQueue(_ videoID: String, listId: String = "") {
-    request_data = {LIST_ID: list_id,
-                ACTION: ACTION_SET_PLAYLIST,
-                CURRENT_TIME: "0",
-                CURRENT_INDEX: -1,
-                AUDIO_ONLY: "false",
-                VIDEO_ID: video_id,
-                COUNT: 1, }
-
-    request_data = self._format_session_params(request_data)
-    url_params = {SID: self._sid, GSESSIONID: self._gsession_id,
-                  RID: self._rid, VER: 8, CVER: 1}
-    self._do_post(BIND_URL, data=request_data, headers={LOUNGE_ID_HEADER: self._lounge_token},
-                  session_request=True, params=url_params)
-  }
-
-  /**
-    Sends actions for an established queue.
-  */
-  private func queueAction(videoID: String, action: String) {
-        # If nothing is playing actions will work but won"t affect the queue.
-        # This is for binding existing sessions
-        if not self.in_session:
-            self._start_session()
-        else:
-            # There is a bug that causes session to get out of sync after about 30 seconds. Binding again works.
-            # Binding for each session request has a pretty big performance impact
-            self._bind()
-
-        request_data = {ACTION: action,
-                        VIDEO_ID: video_id,
-                        COUNT: 1}
-
-        request_data = self._format_session_params(request_data)
-        url_params = {SID: self._sid, GSESSIONID: self._gsession_id, RID: self._rid, VER: 8, CVER: 1}
-        self._do_post(BIND_URL, data=request_data, headers={LOUNGE_ID_HEADER: self._lounge_token},
-                      session_request=True, params=url_params)
+  
+  private var inSession: Bool {
+    return (gsessionID != nil && loungeToken != nil)
   }
   
-  override func handleResponse(_ json: JSON, sourceId: String) {
-    guard let rawType = json["type"].string else { return }
-    
-    guard let type = CastMessageType(rawValue: rawType) else {
-      print("Unknown type: \(rawType)")
-      print(json)
+  private func formatSessionParams(_ params: [String: Any]) -> [String: Any] {
+    var reqCount = "req\(reqCount)"
+    return {req_count + k if k.startswith("_") else k: v for k, v in param_dict.items()}
+  }
+
+  private func fetchScreenIDIfNecessary(for app: CastApp, completion: @escaping (Result<String, CastError>) -> Void) {
+    if let screenID = screenID {
+      completion(Result(success: screenID))
       return
     }
-    
-    switch type {
-    case .mdxSessionStatus:
-      // TODO: Check that this is getting the screenId from the response payload
-      guard let screenId = json["data"]?["screenId"] else { return }
-      delegate?.channel(self, didRecieve: )
-    default:
-      print(rawType)
-    }
-  }
-  
-  public func requestMediaStatus(for app: CastApp, completion: ((Result<CastMediaStatus, CastError>) -> Void)? = nil) {
-    let payload: [String: Any] = [
-      CastJSONPayloadKeys.type: CastMessageType.statusRequest.rawValue,
-      CastJSONPayloadKeys.sessionId: app.sessionId
-    ]
-    
-    let request = requestDispatcher.request(withNamespace: namespace,
-                                       destinationId: app.transportId,
-                                       payload: payload)
-    
-    if let completion = completion {
-      send(request) { result in
-        switch result {
-        case .success(let json):
-          completion(Result(value: CastMediaStatus(json: json)))
-          
-        case .failure(let error):
-          completion(Result(error: error))
-        }
-      }
-    } else {
-      send(request)
-    }
-  }
-  
-  public func sendPause(for app: CastApp, mediaSessionId: Int) {
-    send(.pause, for: app, mediaSessionId: mediaSessionId)
-  }
-  
-  public func sendPlay(for app: CastApp, mediaSessionId: Int) {
-    send(.play, for: app, mediaSessionId: mediaSessionId)
-  }
-  
-  public func sendStop(for app: CastApp, mediaSessionId: Int) {
-    send(.stop, for: app, mediaSessionId: mediaSessionId)
-  }
-  
-  public func sendSeek(to currentTime: Float, for app: CastApp, mediaSessionId: Int) {
-    let payload: [String: Any] = [
-      CastJSONPayloadKeys.type: CastMessageType.seek.rawValue,
-      CastJSONPayloadKeys.sessionId: app.sessionId,
-      CastJSONPayloadKeys.currentTime: currentTime,
-      CastJSONPayloadKeys.mediaSessionId: mediaSessionId
-    ]
-    
-    let request = requestDispatcher.request(withNamespace: namespace,
-                                 destinationId: app.transportId,
-                                 payload: payload)
-    
-    send(request)
-  }
-  
-  private func send(_ message: CastMessageType, for app: CastApp, mediaSessionId: Int) {
-    let payload: [String: Any] = [
-      CastJSONPayloadKeys.type: message.rawValue,
-      CastJSONPayloadKeys.mediaSessionId: mediaSessionId
-    ]
-    
-    let request = requestDispatcher.request(withNamespace: namespace,
-                                 destinationId: app.transportId,
-                                 payload: payload)
-    
-    send(request)
-  }
-  
-  public func load(media: CastMedia, with app: CastApp, completion: @escaping (Result<CastMediaStatus, CastError>) -> Void) {
-    var payload = media.dict
-    payload[CastJSONPayloadKeys.type] = CastMessageType.load.rawValue
-    payload[CastJSONPayloadKeys.sessionId] = app.sessionId
-    
-    let request = requestDispatcher.request(withNamespace: namespace,
-                                       destinationId: app.transportId,
-                                       payload: payload)
 
-    send(request) { result in
-      switch result {
-      case .success(let json):
-        guard let status = json["status"].array?.first else { return }
-        
-        completion(Result(value: CastMediaStatus(json: status)))
-        
-      case .failure(let error):
-        completion(Result(error: CastError.load(error.localizedDescription)))
-      }
-    }
-  }
-
-  private func startSessionIfNecessary(for app: CastApp, completion: @escaping (Result<CastMediaStatus, CastError>) -> Void) {
     let payload: [String: Any] = [
       CastJSONPayloadKeys.type: CastMessageType.getScreenID.rawValue,
       CastJSONPayloadKeys.sessionId: app.sessionId
     ]
     
-    let request = requestDispatcher.request(withNamespace: namespace,
-                                 destinationId: app.transportId,
-                                 payload: payload)
+    let request = requestDispatcher.request(
+      withNamespace: namespace,
+      destinationId: app.transportId,
+      payload: payload
+    )
 
     send(request) { result in
       switch result {
       case .success(let json):
         guard let screenId = json["data"]?["screenId"] else { return }
+        self.screenID = screenId
+        completion(Result(success: screenId))
       case .failure(let error):
         completion(Result(error: CastError.load(error.localizedDescription)))
       }
